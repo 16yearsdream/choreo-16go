4ba936224e9fae2d16e79adca762045369f5f440
16yearsdream <poulli@zohomail.com>
2024-12-20 00:26:07 -0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,9 +6,9 @@ const UUID = process.env.UUID || '81e22980-74d7-42fc-a868-b2fefede79a2'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || 'N9BVoBfucVIrIzCBt8';   // 没用哪吒，把这个参数空着
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo16-sixone.sixteenbvcg.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNDBhZTJlZDE4MWJmMDI0MjgwZWUzNWE1YTZhZDEzZGMiLCJ0IjoiNTlmNDQ4MGItZTYzMy00MGJkLTlhMzEtYmZhYzE3MjE3ZWQ4IiwicyI6Ik5EWm1NR1prTVRBdFpXTm1ZeTAwTW1ZMExUa3lOalF0TkRReE1UTTBaVFV4TVRJeSJ9';
-const CFIP = process.env.CFIP || 'na.ma';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'sixteenchoreo1.sixteenbvcg.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNDBhZTJlZDE4MWJmMDI0MjgwZWUzNWE1YTZhZDEzZGMiLCJ0IjoiZjIxN2Q0NjktNTJhMS00NGM1LThlODctMDhlZGU0YzQ5Mzk0IiwicyI6IlpUQmpNakV3TXpFdE5HSXdNaTAwWXpCa0xXRm1ZVFl0Wm1VMVlqQmhOV1JqWWpneSJ9';
+const CFIP = process.env.CFIP || 'trodo.com';
 const NAME = process.env.NAME || 'Choreo';
 
 // root route
